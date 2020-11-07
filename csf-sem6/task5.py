e05db0bd1de58edf11d4a934c7bc1b390f3eaa1a
00009985 <00009985wiut@gmail.com>
2020-11-07 19:16:30 +0500

    task5 done

--- csf-sem6/task5.py
+++ csf-sem6/task5.py
@@ -1,1 +1,9 @@
-text
+def find(word, letter, index):
+     while index < len(word):
+        if word[index] == letter:
+          return index
+        index = index + 1
+     return -1
+
+
+print(find("computer", "p", 0))
